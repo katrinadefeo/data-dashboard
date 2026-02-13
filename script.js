db3a444b1a62db5fd7b4f2e3cdf5312d6999ae2c
Katrina DeFeo <kdefeo97@gmail.com>
2026-02-12 20:15:50 -0600

    Move buttons outside container and center with flexbox
    created a reset button and moved all buttons to the center.
    
    Also add styling to CSS
    
    Reset Button resets value to 128 in JS file

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let dataCount = 128;
 const container = document.getElementById("data-container");
 const paragraph = container.querySelector("p");
 const button = document.getElementById("update-btn");
+const resetButton = document.getElementById("reset-btn");
 
 console.log("button found?", button);
 console.log("paragraph found?", paragraph);
@@ -12,6 +13,12 @@ paragraph.textContent = `Total Records: ${dataCount}`;
 button.addEventListener("click", function () {
   dataCount++;
   paragraph.textContent = `Total Records: ${dataCount}`;
+});
+
+resetButton.addEventListener("click", function () {
+  dataCount = 128;
+  paragraph.textContent = `Total Records: ${dataCount}`;
+
 });
 
 
